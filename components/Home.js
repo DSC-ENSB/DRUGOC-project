428bcdec00eb4496c2b7b18ae1e3378cfa9d52e4
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-14 12:49:35 +0100

    create and update home Component

---
+++ components/Home.js
@@ -0,0 +1,13 @@
+import React ,{ Component } from 'react'
+
+class Home extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+        return (
+            <h1>Hello From Home</h1>
+        )
+    }
+}
+export default Home;
