ccfebd70e88c416d42cd91cb3c01124e851842f7
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-14 12:50:05 +0100

    create and update Sign-up Component

---
+++ components/SignUp.js
@@ -0,0 +1,13 @@
+import React ,{ Component } from 'react'
+
+class SignUp extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+        return (
+            <h1>Hello From Sign-UP</h1>
+        )
+    }
+}
+export default SignUp;
