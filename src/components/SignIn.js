60a35cc2b2169699a66648fc616dc8bec0b9eaa0
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-16 18:16:19 +0100

    Create and update Signin file

---
+++ src/components/SignIn.js
@@ -0,0 +1,13 @@
+import React ,{ Component } from 'react'
+
+class SignIn extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+        return (
+            <h1>Hello From Sign-In</h1>
+        )
+    }
+}
+export default SignIn;
