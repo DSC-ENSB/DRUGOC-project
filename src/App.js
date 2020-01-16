65e3a157e24bed27b0365033e7d5ecdbe8b7db85
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-16 18:14:57 +0100

    Restructred Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+//import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src={logo} height="100" width="100"></img>
-        <h1 className="alert alert-primary">Hello World!</h1>
-      </header>
-    </div>
+    <React.Fragment>
+      <SignIn />
+      <SignUp />
+    </React.Fragment>
   );
 }
 
