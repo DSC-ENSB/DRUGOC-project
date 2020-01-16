557886e23e47204e128048052fe7f5f58b29b066
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-16 18:17:28 +0100

    Create Nested Compontent Nav  for SignUp file

---
+++ src/components/signupComp/Nav.js
@@ -0,0 +1,15 @@
+import React , { Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+class Nav extends Component{
+    render(){
+        return(
+            <div className="s-header__navbar-row">
+                <div className="s-header__navbar-row-col">Hello </div>
+                <div className="s-header__navbar-row-col">Hi</div>
+            </div>
+        )
+    }
+}
+export default Nav;
