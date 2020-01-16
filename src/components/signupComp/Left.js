fdf613ee8a1900ad07a75507ec788f88c34f540e
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-16 18:17:15 +0100

    Create Nested Compontent for SignUp file

---
+++ src/components/signupComp/Left.js
@@ -0,0 +1,13 @@
+import React , { Component} from 'react'
+
+class Left extends Component{
+    render(){
+        return(
+            <div>
+               <h1>Centre National de Pharmacovigilance et de Matériovigilance</h1>
+               <p>Good Look with montserrat Font So Use It</p> 
+            </div>
+        )
+    }
+}
+export default Left;
