afd35a2cd9afa86eab2cde4b62a1758768a4b7b2
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-18 20:15:33 +0100

    Update Sign-Up component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,19 +1,20 @@
-import React ,{ Component } from 'react'
+import React from 'react'
 import Nav from './signupComp/Nav'
 import Left from './signupComp/Left'
 import Right from './signupComp/Right'
-class SignUp extends Component {
-    constructor(){
-        super();
-    }
-    render(){
+function SignUp(){
         return (
-            <div>
+        <React.Fragment>
                 <Nav />
+            <div className="container h-100 g-ver-center--mdg-padding-y-100--xs">
+                <div className="row d-flex align-items-center h-100 g-hor-centered-row--md g-margin-t-30--xs g-margin-t-20--sm"> 
                 <Left />
+                <div className="col-lg-2"></div>
                 <Right />
+                </div>
             </div>
+        </React.Fragment>
         )
     }
-}
+
 export default SignUp;
