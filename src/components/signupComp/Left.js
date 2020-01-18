19e580d5ad2bff15e2ec6cf08c9b75d5f1fd9a46
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-18 20:15:54 +0100

    Style Modification

--- src/components/signupComp/Left.js
+++ src/components/signupComp/Left.js
@@ -3,9 +3,9 @@ import React , { Component} from 'react'
 class Left extends Component{
     render(){
         return(
-            <div>
-               <h1>Centre National de Pharmacovigilance et de Matériovigilance</h1>
-               <p>Good Look with montserrat Font So Use It</p> 
+            <div className="col-lg-6 col-sm-6 g-text-left--md g-margin-b-60--xs g-margin-b-0--md">
+               <h4 style={{color : 'white','fontSize':26}}>Centre National de Pharmacovigilance et de Matériovigilance</h4>
+               <p style={{color:"white"}}>Good Look with montserrat Font So Use It</p> 
             </div>
         )
     }
