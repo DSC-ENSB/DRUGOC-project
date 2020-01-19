07d02297a3bb9a185fb62d37fa47f09417680d9f
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-20 00:12:09 +0100

    restrcture SignUp Component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import Nav from './signupComp/Nav'
 import Left from './signupComp/Left'
 import Right from './signupComp/Right'
-function SignUp(){
+import SignIn from './SignIn'
+function SignUp(props){
         return (
         <React.Fragment>
-                <Nav />
-            <div className="container h-100 g-ver-center--mdg-padding-y-100--xs">
-                <div className="row d-flex align-items-center h-100 g-hor-centered-row--md g-margin-t-30--xs g-margin-t-20--sm"> 
+                <div className="flex-left-and-right">
                 <Left />
-                <div className="col-lg-2"></div>
-                <Right />
+                <SignIn status={props.isClicked}/>
+                <Right status={props.isClicked}/> 
                 </div>
-            </div>
         </React.Fragment>
         )
     }
