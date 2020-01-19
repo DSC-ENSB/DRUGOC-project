cdcbe29fa46680f8ac76192cb25d51090876c38d
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-20 00:10:45 +0100

    update app to handle and send props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
-import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 //import Home from './components/Home';
-
-function App() {
+import Nav from './components/Nav';
+//import { BrowserRouter as Router,Switch,Route, Link} from "react-router-dom";
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      isClicked : false
+    }
+    this.handleclick = this.handleclick.bind(this);
+  }
+  handleclick(){this.setState(prevState => ({isClicked : !prevState.isClicked}) )}
+  render() {
   return (
     <React.Fragment>
-      <SignUp />
-      <SignIn />
+      <Nav handleclick={this.handleclick} isClicked={this.state.isClicked}/>
+      <SignUp isClicked={this.state.isClicked}/>    
     </React.Fragment>
   );
+}
 }
 
 export default App;
