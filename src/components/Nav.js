4f2248874f6e3feda6d53f63af0c17ace1109ee2
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-20 00:09:40 +0100

    handle button click

---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React , { Component} from 'react'
+//import { BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+
+class Nav extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <header>
+                <div className="flex-nav">
+                    <div className="flex-nav-right" style={{fontSize:32}}>CNPM</div>
+                    <div className="flex-nav-left">
+                        <button 
+                        onClick={this.props.handleclick}
+                        id="log-btn" 
+                        style={{color:'white',backgroundColor:'transparent',border:'none'}}>
+                        {this.props.isClicked?'SIGN UP':'LOG IN'}
+                        </button>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
+export default Nav;
