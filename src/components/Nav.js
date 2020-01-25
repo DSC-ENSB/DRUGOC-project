7b87193e67bb5ad903d52c707d5dae0fea88f7d0
Salem-Abderaouf <salamasalem84@gmail.com>
2020-01-25 15:06:18 +0100

    remove useless Constructor

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,6 @@ import React , { Component} from 'react'
 //import { BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 
 class Nav extends Component{
-    constructor(props){
-        super(props);
-    }
     render(){
         return(
             <header>
