60d7e2f413d66a174659527416e4f8c2a682b00d
Salem-Abderaouf <salamasalem84@gmail.com>
2020-02-06 12:35:35 +0100

    create Profile child Component

---
+++ src/components/ProfileComp/PharmacoTool.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+class PharmacoTool extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            ddmt:''
+        }
+        this.handlchange = this.handlchange.bind(this)
+    }
+    handlchange(event){
+        const {name ,value } = event.target
+        this.setState({
+            [name]:value
+        })
+    }
+    render(){
+        return(
+            <div className={this.props.status?"pharma raised":'hide raised'}>
+            <input 
+            type='text'
+            placeholder='DCI'
+            />
+            <input 
+            type='number'
+            placeholder='N°lot'
+            />
+            <input 
+            type='text'
+            placeholder='Posology'
+            />
+            <input 
+            type='date'
+            placeholder='DAEI'
+            />
+            <input 
+            type='date'
+            placeholder='DEM'
+            />
+            <input 
+            value={this.state.ddmt}
+            name='ddmt'
+            type='text'
+            onChange={this.handlchange}
+            placeholder='DDMT'
+            />
+            <input 
+            type='text'
+            placeholder='Voie '
+            />
+           <h3>{this.state.ddmt}</h3> 
+            </div>
+        )
+    }
+}
+export default PharmacoTool;
