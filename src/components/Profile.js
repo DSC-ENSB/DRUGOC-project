42983392cd5cc87b7027a751a1ab0f88037a20cc
Salem-Abderaouf <salamasalem84@gmail.com>
2020-02-06 12:34:58 +0100

    create and update Profile Component

---
+++ src/components/Profile.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import SideBar from './ProfileComp/SideBar'
+import Introduction from './ProfileComp/Introduction'
+import PharmacoTool from './ProfileComp/PharmacoTool'
+
+
+class Profile extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            headerShowen : false
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    componentDidMount(){
+        const head = document.getElementsByTagName('header')[0]
+        head.classList.add('hide')
+        const body = document.getElementsByTagName('body')[0]
+        body.style.backgroundImage = "none"
+    }
+    handleClick(){
+        this.setState(prevState=> ({
+            headerShowen : !prevState.headerShowen
+        })
+    )}
+    render(){
+        return (
+            <div className="Profile">
+                <SideBar/>
+                <Introduction handleClick={this.handleClick} status={this.state.headerShowen}/>
+                <PharmacoTool status={this.state.headerShowen}/>
+            </div>
+        )
+    }
+}
+export default Profile;
