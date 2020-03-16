d06b83453576c3b9910bec3f5c717f06ffe2d4ca
Salem-Abderaouf <salamasalem84@gmail.com>
2020-03-16 17:22:18 +0100

    update welcome message

--- src/components/ProfileComp/Introduction.js
+++ src/components/ProfileComp/Introduction.js
@@ -10,7 +10,7 @@ class Introduction extends React.Component{
             <div className={this.props.status?'hide':"Intro"}>
             <h3>Welcome</h3>
             <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            DrugOC est un outil informatique innovant destiné au suivi de la pharmacovigilance en regroupant les déclarations volontaires des praticiens professionnel de santé et/ou patients qui rencontrent des effets indésirables dû à la prise d'un médicament. L’ensemble de manière simple, complète et rapide, en prenant en compte l’ensemble des paramètres et variables nécessaires. DrugOC facilite la collecte et la soumission des données au CNPM en substituant ainsi le travail des centres régionaux.
             </p>
             <button 
             id="btn" 
