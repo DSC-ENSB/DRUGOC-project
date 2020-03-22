6d5f4644bb2c4ba6053392683385b6ce65973d77
Salem-Abderaouf <salamasalem84@gmail.com>
2020-03-22 11:30:54 +0100

    indepandent python script to orgnaize data files

---
+++ DrugOC-master/edit.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from itertools import islice 
+import json
+
+i=0
+data=[]
+file = './meddra_all_se.tsv'
+with open(file,encoding="UTF-8")as f:
+    for line in islice(f, 0, None): 
+        l=line.split("\t")
+        i = i + 1
+        data.append(l[-1])
+        if i ==33092:
+            break
+
+with open('effetsInd','w')as json_file:
+    file_json = json.dump(data,json_file)
