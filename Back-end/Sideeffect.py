8bd70b74c19c48f19ebe176d2f5fc0540e2f653b
Ala Eddine BOUDEMIA <alaeddine-boudemia@hotmail.com>
2020-03-23 20:08:14 +0100

    final modification

--- Back-end/Sideeffect.py
+++ Back-end/Sideeffect.py
@@ -2,7 +2,7 @@ from itertools import islice
 
 def sideEffect(side_effect):
 
-	with open("side_effects.tsv", "r", encoding="ISO-8859-1") as f:
+	with open("side_effects.tsv.csv", "r", encoding="ISO-8859-3") as f:
 		for line in islice(f, 0, None): 
 			l = line.split("\t")
 			l[1] = l[1].replace('\n', '')
