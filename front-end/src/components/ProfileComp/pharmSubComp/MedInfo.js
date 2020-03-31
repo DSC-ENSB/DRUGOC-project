c1fc9440442de16d9c62c55ebbe399fb1791b0e8
Salem-Abderaouf <salamasalem84@gmail.com>
2020-03-31 10:34:26 +0100

    Remove useless inputs

--- front-end/src/components/ProfileComp/pharmSubComp/MedInfo.js
+++ front-end/src/components/ProfileComp/pharmSubComp/MedInfo.js
@@ -4,102 +4,85 @@ import React ,{Component}from 'react'
 export default class MedInfo extends Component {
     constructor(props){
         super(props)
+        this.state = {
+          details:''
+        }
         this.showInfo = this.showInfo.bind(this)
+        this.HideInfo = this.HideInfo.bind(this)
     }
     showInfo(type){
-        let info;
         const dataBanner = document.querySelector('.data-details')
         dataBanner.classList.add('show')
         switch (type) {
             case 'dem':
-                info = "date D'exposition Au Medicament"
-                //this.setState({details:info})
+                this.setState({details:"date D'exposition Au Medicament"})
                 break
             case 'daei':
-                info = "data D'apparition De leffetIndiserable"
-                //this.setState({details:info})
+                this.setState({details:"data D'apparition De l'effet Indisérable"})
                 break
             case 'ddmt':
-                info = "date Darret Ou Modification Du Traitement "
-                //this.setState({details:info})
+                this.setState({details:"date D'arret Ou Modification Du Traitement "})
                 break;
           }
     }
+    HideInfo(){
+      const dataBanner = document.querySelector('.data-details')
+      dataBanner.classList.remove('show')
+    }
     render(){
     return(
-    <section className="meds-info">
-    {this.props.meds.map((elem,i)=>(
-    <form className="pharmatoolin" method="POST">
-    <h6 style={{paddingTop:'30px'}}>{this.props.meds[i].DCI}</h6>
-    <input 
-    type='text'
-    placeholder='   Posology'
-    className="pharmaInput"
-    required={true}
-    value={elem.posology}
-    onChange={event => {
-        this.props.meds[i].posology = event.target.value;
-        this.setState([...this.props.meds[i].posology]);
-      }}
-    />
-    <br></br>
-    <input 
-    type='text'
-    placeholder='   Voie'
-    className="pharmaInput"
-    required={true}
-    onChange={event => {
-        this.props.meds[i].VI = event.target.value;
-        this.setState([...this.props.meds[i].VI]);
-      }}
-    value={elem.VI}
-    />
-    <br></br>
-    <input 
-    type='number'
-    placeholder='   N°lot'
-    className="pharmaInput"
-    value={elem.nomberDelot}
-    onChange={event => {
-        this.props.meds[i].nomberDelot = event.target.value;
-        this.setState([...this.props.meds[i].nomberDelot]);
-      }}
-    />
-    <br></br>
-    <input 
-    type='date'
-    placeholder='DAEI'
-    value={elem.dataDapparitionDeleffetIndiserable}
-    onFocus={(e) =>this.showInfo('daei',e)}
-    onChange={event => {
-        this.props.meds[i].dataDapparitionDeleffetIndiserable = event.target.value;
-        this.setState([...this.props.meds[i].dataDapparitionDeleffetIndiserable]);
-      }}
-    /><br></br>
-    <input 
-    type='date'
-    value={elem.dateDexpositionAuMedicament}
-    placeholder='DEM'
-    onChange={event => {
-        this.props.meds[i].dateDexpositionAuMedicament = event.target.value;
-        this.setState([...this.props.meds[i].dateDexpositionAuMedicament]);
-      }}
-    onFocus={(e) =>this.showInfo('dem',e)}
-    /><br></br>
-    <input 
-    value={elem.dateDarretOuModificationDuTraitement}
-    name='ddmt'
-    type='date'
-    onChange={event => {
-        this.props.meds[i].dateDarretOuModificationDuTraitement = event.target.value;
-        this.setState([...this.props.meds[i].dateDarretOuModificationDuTraitement]);
-      }}
-    placeholder='DDMT'
-    onFocus={(e) =>this.showInfo('ddmt',e)}
-    /><br></br>
-</form>
-))}
-</section>
+        <section className="meds-info">
+        {this.props.meds.map((elem,i)=>(
+        <form className="pharmatoolin" method="POST">
+        <h6 style={{paddingTop:'30px'}}>{this.props.meds[i].DCI}</h6>
+        <input 
+        type='number'
+        placeholder='   N°lot'
+        className="pharmaInput"
+        value={elem.nomberDelot}
+        onChange={event => {
+            this.props.meds[i].nomberDelot = event.target.value;
+            this.setState([...this.props.meds[i].nomberDelot]);
+          }}
+        />
+        <br></br>
+        <input 
+        type='date'
+        placeholder='DAEI'
+        value={elem.dateDapparitionDeLeffetIndiserable}
+        onFocus={(e) =>this.showInfo('daei',e)}
+        onChange={event => {
+            this.props.meds[i].dateDapparitionDeLeffetIndiserable = event.target.value;
+            this.setState([...this.props.meds[i].dateDapparitionDeLeffetIndiserable]);
+          }}
+        onBlur={(e) =>this.HideInfo()}
+        /><br></br>
+        <input 
+        type='date'
+        value={elem.dateDexpositionAuMedicament}
+        placeholder='DEM'
+        onChange={event => {
+            this.props.meds[i].dateDexpositionAuMedicament = event.target.value;
+            this.setState([...this.props.meds[i].dateDexpositionAuMedicament]);
+          }}
+        onFocus={(e) =>this.showInfo('dem',e)}
+        onBlur={(e) =>this.HideInfo()}
+        /><br></br>
+        <input 
+        value={elem.dateDarretOuModificationDuTraitement}
+        name='ddmt'
+        type='date'
+        onChange={event => {
+            this.props.meds[i].dateDarretOuModificationDuTraitement = event.target.value;
+            this.setState([...this.props.meds[i].dateDarretOuModificationDuTraitement]);
+          }}
+        placeholder='DDMT'
+        onFocus={(e) =>this.showInfo('ddmt',e)}
+        onBlur={(e) =>this.HideInfo()}
+        /><br></br>
+    </form>
+    ))}
+    </section>
 
         )
     }
