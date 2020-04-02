47219ef43bf9116b6538cca97bab01b3ac19cf9f
Salem-Abderaouf <salamasalem84@gmail.com>
2020-04-02 23:01:45 +0100

    update recieved props and handle it

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,5 +1,5 @@
 import React , { Component} from 'react'
-//import { BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import { BrowserRouter as Router,Link} from "react-router-dom";
 
 class Nav extends Component{
     render(){
@@ -8,12 +8,23 @@ class Nav extends Component{
                 <div className="flex-nav">
                     <div className="flex-nav-right" style={{fontSize:32}}>DRUGOC</div>
                     <div className="flex-nav-left">
+                        {this.props.isClicked?
+                        <Link to="/sign-up"><button 
+                        onClick={this.props.handleclick}
+                        id="log-btn" 
+                        style={{color:'white',backgroundColor:'transparent',border:'none'}}>
+                        SIGN UP
+                        </button>
+                        </Link>:
+                        <Link to="/log-in">
                         <button 
                         onClick={this.props.handleclick}
                         id="log-btn" 
                         style={{color:'white',backgroundColor:'transparent',border:'none'}}>
-                        {this.props.isClicked?'SIGN UP':'LOG IN'}
+                        LOG IN
                         </button>
+                        </Link>
+                        }
                     </div>
                 </div>
             </header>
