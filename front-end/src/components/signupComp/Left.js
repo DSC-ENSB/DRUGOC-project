3d7282562a00589e08c8cf2b299b4d00f63664ff
Salem-Abderaouf <salamasalem84@gmail.com>
2020-04-04 23:12:38 +0100

    update style

--- front-end/src/components/signupComp/Left.js
+++ front-end/src/components/signupComp/Left.js
@@ -4,8 +4,8 @@ class Left extends Component{
     render(){
         return(
             <div className="flex-left">
-               <h4 style={{fontSize:32,fontWeight: 'bold'}}>DRUG <br></br>OVERSIGHT  AND <br></br>CONTROLE</h4>
-               <p>Good Look with montserrat Font So Use It</p> 
+               <h4 style={{fontSize:32,fontWeight: 'bold',color:"#000"}}>DRUG <br></br>OVERSIGHT  AND <br></br>CONTROLE</h4>
+               <p style={{color:"#764abc"}}>Good Look with montserrat Font So Use It</p> 
             </div>
         )
     }
