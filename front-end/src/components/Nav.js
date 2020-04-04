e29359561a9cc7871e4b4ba34afe0ca5befbebff
Salem-Abderaouf <salamasalem84@gmail.com>
2020-04-04 23:17:16 +0100

    Replace Component
    Replace Nav Component from Global root into Under Sign-up and Sign-in Components
    
    Signed-off-by: Salem-Abderaouf <salamasalem84@gmail.com>

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,5 +1,5 @@
 import React , { Component} from 'react'
-import { BrowserRouter as Router,Link} from "react-router-dom";
+import { Link} from "react-router-dom";
 
 class Nav extends Component{
     render(){
@@ -9,19 +9,18 @@ class Nav extends Component{
                     <div className="flex-nav-right" style={{fontSize:32}}>DRUGOC</div>
                     <div className="flex-nav-left">
                         {this.props.isClicked?
-                        <Link to="/sign-up"><button 
-                        onClick={this.props.handleclick}
-                        id="log-btn" 
-                        style={{color:'white',backgroundColor:'transparent',border:'none'}}>
-                        SIGN UP
-                        </button>
-                        </Link>:
                         <Link to="/log-in">
                         <button 
-                        onClick={this.props.handleclick}
-                        id="log-btn" 
-                        style={{color:'white',backgroundColor:'transparent',border:'none'}}>
+                        className="nav-link" 
+                        style={{color:'#764abc',backgroundColor:'transparent',border:'none'}}>
                         LOG IN
+                        </button>
+                        </Link>:
+                        <Link to="/sign-up">
+                        <button 
+                        className="nav-link" 
+                        style={{color:'#764abc',backgroundColor:'transparent',border:'none'}}>
+                        SIGN UP
                         </button>
                         </Link>
                         }
