1452960dd41dcf7c441be932ef7ebf9416eb5139
Salem-Abderaouf <salamasalem84@gmail.com>
2020-04-04 16:47:49 +0100

    change style

--- front-end/src/components/signupComp/Left.js
+++ front-end/src/components/signupComp/Left.js
@@ -4,7 +4,7 @@ class Left extends Component{
     render(){
         return(
             <div className="flex-left">
-               <h4 style={{fontSize:32,fontWeight: 'bold'}}>Centre National de <br></br>Pharmacovigilance  et <br></br>de Matériovigilance</h4>
+               <h4 style={{fontSize:32,fontWeight: 'bold'}}>DRUG <br></br>OVERSIGHT  AND <br></br>CONTROLE</h4>
                <p>Good Look with montserrat Font So Use It</p> 
             </div>
         )
